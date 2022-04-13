c2544c4b08aec6e9abf84ebca1d09dae56ad96d5
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-04-13 14:42:05 +0900

    fs

--- cote/implementation/Hour.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    count = 0
-    for h in range(n + 1):
-        for m in range(60):
-            for s in range(60):
-                if '3' in str(h)+str(m)+str(s):count+=1
-    print(count)
-
