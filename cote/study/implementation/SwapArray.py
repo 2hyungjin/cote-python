a65fa1fdf59dc43d40604d6a6151457d33bdfd1f
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-04-16 17:03:32 +0900

    배열 원소 바꾸기

---
+++ cote/study/implementation/SwapArray.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    a.sort()
+    b.sort(reverse=True)
+
+    for i in range(m):
+        a[i] = b[i]
+
+    print(sum(a))
