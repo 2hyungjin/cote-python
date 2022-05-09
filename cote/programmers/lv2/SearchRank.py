4bdbd97f0adf2305d8c46897172bc10e0367e17f
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-05-09 17:46:22 +0900

    순위 검색(정확성)

--- cote/programmers/lv2/SearchRank.py
+++ cote/programmers/lv2/SearchRank.py
@@ -1,8 +1,20 @@
-import itertools
+import itertools, collections
 
-languages = ('cpp', 'java', 'python')
-jobs = ('frontend', 'backend')
-careers = ('junior', 'senior')
-foods = ('chicken', 'pizza')
 
-print(set(itertools.combinations()))
+def solution(info, query):
+    result = collections.defaultdict(list)
+    answer = []
+
+    for i in info:
+        lan, job, career, food, grade = i.split()
+
+        for case in itertools.product([lan, '-'], [job, '-'], [career, '-'], [food, '-']):
+            result[''.join(case)].append(int(grade))
+
+    for q in query:
+        q = q.replace('and', '').replace('  ', ' ')
+        lan, job, career, food, grade = q.split()
+
+        answer.append(len([i for i in result[lan + job + career + food] if i >= int(grade)]))
+
+    return answer
