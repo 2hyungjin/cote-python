edea4259489faa89bf765d993d3a177c8d19fd13
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2022-05-10 11:14:52 +0900

    배달

---
+++ cote/programmers/lv2/Delivery.py
@@ -0,0 +1,29 @@
+import heapq
+
+
+def solution(N, road, K):
+    answer = 0
+
+    roads = [1e9] * (N + 1)
+    graph = [[] for _ in range(N + 1)]
+    q = []
+
+    for r in road:
+        graph[r[1]].append((r[0], r[2]))
+        graph[r[0]].append((r[1], r[2]))
+
+    roads[1] = 0
+
+    heapq.heappush(q, (1, 0))
+
+    while q:
+        now, dist = heapq.heappop(q)
+        for i, c in graph[now]:
+            cost = dist + c
+            roads[i] = min(cost, roads[i])
+            if roads[i] == cost:
+                heapq.heappush(q, (i, roads[i]))
+
+    print(roads)
+
+    return len(list(filter(lambda a: a <= K, roads)))
